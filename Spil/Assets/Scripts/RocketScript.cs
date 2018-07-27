46fc0406af1800dce3413f3d0c1ca7f51d5d3034
Kodning <adamkjelstroem@gmail.com>
2018-07-27 17:56:46 +0200

    v2

--- Spil/Assets/Scripts/RocketScript.cs
+++ Spil/Assets/Scripts/RocketScript.cs
@@ -3,7 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RocketScript : MonoBehaviour {
-    public float rotateSpeed = 30;
+    public float rotateSpeed = 3000;
+    public float thrust = 10;
+    public float airResistance = 1;
+    public Rigidbody rb;
+    public string inputHorizontalAxis;
+    public string inputVerticalAxis;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +16,16 @@ public class RocketScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Rotate(0, rotateSpeed * Time.deltaTime, 0);
+
+        transform.Rotate(0, rotateSpeed * Input.GetAxis(inputHorizontalAxis) * Time.deltaTime, 0);
 	}
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(transform.forward * thrust* Input.GetAxis(inputVerticalAxis), ForceMode.Impulse);
+
+
+        rb.AddForce(-rb.velocity.normalized * airResistance * rb.velocity.sqrMagnitude, ForceMode.Impulse);
+
+    }
 }
