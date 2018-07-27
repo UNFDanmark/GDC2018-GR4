5cba1067b57ebc282f9ff71a7d76eb9b73654fb2
Kodning <adamkjelstroem@gmail.com>
2018-07-27 16:41:54 +0200

    it works

---
+++ Spil/Assets/Scripts/RocketScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketScript : MonoBehaviour {
+    public float rotateSpeed = 30;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0, rotateSpeed * Time.deltaTime, 0);
+	}
+}
