5284879495b96c89a4be7b6f6b59096571e5cdbb
Kodning <adamkjelstroem@gmail.com>
2018-07-28 18:11:04 +0200

    sæler virker. toy virker. din mor virker. pointsystem virker.

---
+++ Spil/Assets/Scripts/GameHandlerScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameHandlerScript : MonoBehaviour {
+
+    public int numberOfPlayers = 2;
+
+    public int[] points;
+    public Text pointsText;
+
+    public void Start()
+    {
+        points = new int[numberOfPlayers];
+        Draw();
+    }
+
+    public void AddPoint(int playerNumber)
+    {
+        points[playerNumber-1]++;
+        Draw();
+    }
+    
+    //this is seal
+
+    private void Draw()
+    {
+
+        string s = "";
+        for(int i =0; i < points.Length; i++)
+        {
+            s += "Player " + (i + 1) + ": " + points[i] + " ";
+        }
+
+        pointsText.text = s;
+    }
+
+}
--- Spil/Assets/Scripts/RocketScript.cs
+++ Spil/Assets/Scripts/RocketScript.cs
@@ -10,33 +10,66 @@ public class RocketScript : MonoBehaviour {
     public string inputHorizontalAxis;
     public string inputVerticalAxis;
     public float maxRotationSpeed = 8;
+    public float respawnTime;
+    private float respawnCounter = -1;
+
+    private Vector3 spawnPoint;
+    private Quaternion spawnRotation;
+
+
+    public GameHandlerScript gameHandler;
+    public int playerNumber;
+
 	// Use this for initialization
 	void Start () {
+        spawnPoint = transform.position;
+        spawnRotation = transform.rotation;
 	}
 
-    void OnCollisionEnter(Collision collision)
+    void OnTriggerEnter(Collider collision)
     {
         print("collision with object w tag: " + collision.gameObject.tag);
         if (collision.gameObject.tag.Equals("Seal"))
         {
             //we've hit a seal!
             Destroy(collision.gameObject);
+            gameHandler.AddPoint(playerNumber);
         }
-
         
     }
 
     // Update is called once per frame
     void Update () {
-        //stop automatic rotation
-        rb.angularVelocity = new Vector3(0, 0, 0);
         //rotate via user input
         transform.Rotate(0, rotateSpeed * Input.GetAxis(inputHorizontalAxis) * Time.deltaTime, 0);
-    }
 
+        if (transform.position.y < -5)
+        {
+            
+            
+            if (respawnCounter == -1)
+            {
+                respawnCounter = Time.time + respawnTime;
+            }
+            else if(Time.time>=respawnCounter)
+            {
+                Respawn();
+            }
+        }
+    }
+    private void Respawn()
+    {
+        //player has fallen in the water. it is now dead.
+        //player is teleported to a spawn point.
+        transform.position = spawnPoint;
+        rb.velocity = new Vector3(0, 0, 0);
+        transform.rotation = spawnRotation;
+        respawnCounter = -1;
+    }
+    
     private void FixedUpdate()
     {
-        rb.AddForce(transform.forward * thrust* Input.GetAxis(inputVerticalAxis), ForceMode.Impulse);
+        rb.AddForce(transform.forward * thrust* Input.GetAxis(inputVerticalAxis), ForceMode.Force);
 
 
         //rb.AddForce(-rb.velocity.normalized * airResistance * rb.velocity.sqrMagnitude, ForceMode.Impulse);
